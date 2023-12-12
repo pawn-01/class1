3a2c006fe6d0eea778750157af9590a4f707d989
001pawan <disetting2001@gmail.com>
2023-12-12 19:45:47 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function sum(num1, num2 , func) {
+    let result = num1 + num2;
+    func(result);
+}
+
+function displayResult(data) {
+    console.log("Result of the sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+// You are only allowed to call one function after this
+// How will you displayResult of a sum
+
+
+sum(2,3,displayResult)
